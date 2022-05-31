4c558032c9a2a5fe92f5c426140292b8335d47ea
AlexFariaa <alexcaras1@hotmail.com>
2022-05-30 22:22:38 -0300

    alt

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,35 +1,53 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
-import { CreateGameDto } from "./dto/create-game.dto";
-import { game } from "./entities/game.entities";
-import { GameService } from "./game.service";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateGameDto } from './dto/create-game.dto';
+import { UpdateGameDto } from './dto/update-game.dto';
+import { game } from './entities/game.entities';
+import { GameService } from './game.service';
 
 @ApiTags('games')
-@Controller('Games')
+@Controller('games')
 export class GameController {
-    constructor(private readonly gameservice: GameService) {}
+  constructor(private readonly gameService: GameService) {}
 
-    @Get()
-    @ApiOperation({
-        summary: 'Listar todos os jogos'
-    })
-    findAll(){
-        return this.gameservice.findAll();
-    }
+  @Get()
+  @ApiOperation({
+    summary: 'Listar todos os jogos',
+  })
+  findAll() {
+    return this.gameService.findAll();
+  }
 
-    @Get(':id')
-    @ApiOperation({
-        summary: 'Listar jogo por ID'
-    })
-    findOne(@Param('id') id: string): Promise<game> {
-        return this.gameservice.findOne(id)
-    }
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Listar jogo por ID',
+  })
+  findOne(@Param('id') id: string): Promise<game> {
+    return this.gameService.findOne(id);
+  }
 
-    @Post()
-    @ApiOperation({
-        summary: 'Criar um novo jogo'
-    })
-    create(@Body() dto: CreateGameDto): Promise<game>{
-        return this.gameservice.create(dto);
-    }
+  @Post('create-game')
+  @ApiOperation({
+    summary: 'Criar um novo jogo',
+  })
+  create(@Body() dto: CreateGameDto): Promise<game> {
+    return this.gameService.create(dto);
+  }
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Editar um jogo pelo ID',
+  })
+  update(@Param('id') id: string, @Body() dto: UpdateGameDto): Promise<game> {
+    return this.gameService.update(id,dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Deletar um jogo pelo ID'
+  })
+  delete(@Param('id') id: string){
+    this.gameService.delete(id)
+  }
 }
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -1,29 +1,61 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-import { CreateGameDto } from "./dto/create-game.dto";
-import { game } from "./entities/game.entities";
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateGameDto } from './dto/create-game.dto';
+import { UpdateGameDto } from './dto/update-game.dto';
+import { game } from './entities/game.entities';
 
 @Injectable()
 export class GameService {
-    constructor(private readonly prisma: PrismaService){}
+  constructor(private readonly prisma: PrismaService) {}
 
-    findAll(): Promise <game[]> {
-        return this.prisma.game.findMany()
+  findAll(): Promise<game[]> {
+    return this.prisma.game.findMany();
+  }
+
+  async findById(id: string): Promise<game> {
+    const record = await this.prisma.game.findUnique({ where: { id } });
+    if (!record) {
+      throw new NotFoundException(`Registro com o ID '${id}' não encontrado`);
     }
+    return record;
+  }
 
-    findOne(id: string): Promise<game> {
-        return this.prisma.game.findUnique({
-            where: {id}
-        })
-    }
+  async findOne(id: string): Promise<game> {
+    return this.findById(id);
+  }
 
-    create(dto: CreateGameDto): Promise <game> {
-        const data: game = {...dto};
+  create(dto: CreateGameDto): Promise<game> {
+    const data: game = { ...dto };
 
-        return this.prisma.game.create({
-            data
-        })
+    return this.prisma.game.create({ data }).catch(this.handleError);
+  }
 
-        
-    }
+  async update(id: string, dto: UpdateGameDto): Promise<game> {
+    await this.findById(id);
+
+    const data: Partial<game> = { ...dto };
+
+    return this.prisma.game
+      .update({
+        where: { id },
+        data,
+      })
+      .catch(this.handleError);
+  }
+  async delete(id: string) {
+    await this.findById(id);
+    await this.prisma.game.delete({ where: { id } });
+  }
+
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n');
+    const lastErrorLines = errorLines[errorLines.length - 1]?.trim();
+    throw new UnprocessableEntityException(
+      lastErrorLines || 'Algum erro ocorreu ao executar a operação',
+    );
+  }
 }
