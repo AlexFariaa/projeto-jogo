6b290c4ff6d5b17b644c5ffbd9f759e3ec29e715
AlexFariaa <alexcaras1@hotmail.com>
2022-05-31 00:00:53 -0300

    tentativa de arrumar erro

--- src/game/entities/game.entities.ts
+++ src/game/entities/game.entities.ts
@@ -1,4 +1,11 @@
 export class game {
     id?: string;
     number: number;
+    /*title: string;
+    coverImageUrl: string;
+    description: string;
+    Year: Number;
+    imdbScrore: Number;
+    trailerYouTubeUrl: string;
+    gameplayYoutubeUrl: string;*/
 }
