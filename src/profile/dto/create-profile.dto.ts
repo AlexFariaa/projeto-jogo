48d96bd2002573f13b2fac58ac9e7184593407e1
AlexFariaa <alexcaras1@hotmail.com>
2022-06-06 15:40:17 -0300

    bcrypt

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,13 +1,18 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsUrl } from "class-validator";
 
 export class CreateProfileDto {
+    @IsString()
     @ApiProperty({
-        description: 'Cria um novo Profile',
-        example: {
-            "title": "Seu nome",
-            "imageUrl": "sua foto png"
-        }
+        description: 'Nome do perfil',
+        example: "AlexF"
     })
     title: string;
+
+    @IsUrl()
+    @ApiProperty({
+        description: "Foto de perfil",
+        example: "https://avatars.githubusercontent.com/u/97993854?v=4"
+    })
     imageUrl: string;
 }
